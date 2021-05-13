8c7c40d785288aea41acadc2e54f855e707e5fbf
齋藤百合奈 <pee199605ys@gmail.com>
2021-05-13 20:29:20 +0900

    最初のコミット

---
+++ WhatToBuyNotificationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToBuyNotificationApp
+//
+//  Created by 齋藤百合奈 on 2021/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
